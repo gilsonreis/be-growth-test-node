d1757955defb7ec35a503e698f8fa97ece99dc79
Gilson Reis <gilson.reis@jamesdelivery.com.br>
2022-02-15 17:14:06 -0300

    Conexão com API e consulta de playlists

--- backend/src/Routers/SpotifyRouters.ts
+++ backend/src/Routers/SpotifyRouters.ts
@@ -4,5 +4,6 @@ import SpotifyController from '@controllers/SpotifyController'
 const SpotifyRouter:Router = Router()
 
 SpotifyRouter.get('/spotify', SpotifyController.index)
+SpotifyRouter.post('/spotify/get-playlist', SpotifyController.getPlayLists)
 
 export default SpotifyRouter
