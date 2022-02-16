cd6b148bdffde9c0f8db1706e82ab9f4926ccd48
Gilson Reis <gilson.reis@jamesdelivery.com.br>
2022-02-15 21:14:37 -0300

    Criacao de teste

--- src/App/Controllers/SpotifyController.ts
+++ src/App/Controllers/SpotifyController.ts
@@ -1,31 +1,15 @@
 import { Request, Response } from 'express'
 import { getCustomRepository } from 'typeorm'
 import BeerRepository from '@repositories/BeerRepository'
-import axios from 'axios'
-import { request as request_http } from 'http'
 import SpotifyService from '@services/SpotifyService'
 
 class SpotifyController {
-  public async index (request: Request, response: Response) {
-    const beerRepository = getCustomRepository(BeerRepository)
-    const beer = await beerRepository.getBeerByTemp(-2)
-
-    return response.status(200).json({
-      status: 'success',
-      data:
-        {
-          beer
-        }
-    })
-  }
-
   public async getPlayLists (request: Request, response: Response) {
     const { temp } = request.body
     const beerRepository = getCustomRepository(BeerRepository)
     const beer = await beerRepository.getBeerByTemp(temp)
-
     const beer_type = beer[0].beer_type
-    console.log(beer_type)
+
     const spotifyService = new SpotifyService()
 
     const cred = await spotifyService.getCredencial()
